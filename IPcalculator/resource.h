22746a6213c29890a906bad2628b42684b86f921
kit <kit@DESKTOP-EHKDJE1>
2026-05-23 22:24:21 +0300

    TODO:IP-prefix

--- IPcalculator/resource.h
+++ IPcalculator/resource.h
@@ -3,13 +3,13 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG_MAIN                 7
-#define IDC_IPADDRESS1                  1001
 #define IDC_IPADDRESS                   1001
 #define IDC_IPMASK                      1002
-#define IDC_SPIN_PREFIX                 1003
-#define IDC_EDIT_PREFIX                 1004
 #define IDC_STATIC_IP                   1005
 #define IDC_STATIC_MASK                 1006
+#define IDC_EDIT_PREFIX                 1007
+#define IDC_SPIN_PREFIX                 1008
+#define IDC_SPIN2                       1009
 
 // Next default values for new objects
 // 
@@ -17,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
