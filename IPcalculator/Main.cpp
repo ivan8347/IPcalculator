285dfc80d3db656567dab494bd5e4e37a0d41e29
kit <kit@DESKTOP-EHKDJE1>
2026-05-30 10:27:28 +0300

    mask_depended_done

--- IPcalculator/Main.cpp
+++ IPcalculator/Main.cpp
@@ -28,6 +28,8 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		DWORD dwIPaddress	= 0;
 		DWORD dwIPmask		= 0;
 		DWORD dwPrefix		= 0;
+		CHAR szPrefix[3] = {};
+
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
@@ -47,23 +49,28 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
 				
-				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+				/*SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
 				dwPrefix = UINT_MAX;
 				for (dwPrefix = 0; dwIPmask; dwPrefix++)dwIPmask <<= 1;
-				CHAR szPrefix[3] = {};
 				sprintf(szPrefix, "%i", dwPrefix);
-				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szPrefix);
-
-				
+				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szPrefix);*/
 			}
 			break;
+
+
 		case IDC_EDIT_PREFIX:
-			if (HIWORD(wParam) == EN_CHANGE)
-			{
-				
-			}
+
+			SendMessage(hEditPrefix, WM_GETTEXT, 3, (LPARAM)&szPrefix);
+			dwPrefix = atoi(szPrefix);
+			dwIPmask = UINT_MAX;
+			for (int i = 0; i < 32 - dwPrefix; i++)dwIPmask <<= 1;
+			SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
 			break;
 
+		case IDC_BUTTON_RESET:
+			SendMessage(hIPaddress, IPM_CLEARADDRESS, 0, 0);
+			SendMessage(hIPmask, IPM_CLEARADDRESS, 0, 0);
+			break;
 
 		case IDOK:
 			break;
@@ -71,6 +78,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 	}
 			break;
+	case WM_NOTIFY:
+	{
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		DWORD dwIPmask = 0;
+		DWORD dwPrefix = 0;
+		CHAR szPrefix[3] = {};
+
+		SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+		dwPrefix = UINT_MAX;
+		for (dwPrefix = 0; dwIPmask; dwPrefix++)dwIPmask <<= 1;
+		sprintf(szPrefix, "%i", dwPrefix);
+		SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szPrefix);
+
+	}
+			break;
+
 	case WM_CLOSE:EndDialog(hwnd, 0);
 		break;
 	}
